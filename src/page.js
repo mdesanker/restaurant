c08440d6675ef03973ed67983cbd0a010a6f0383
Michael Desanker <mdesanker@gmail.com>
2021-10-12 14:23:47 -0400

    feat: generate nav links with JS

--- src/page.js
+++ src/page.js
@@ -9,12 +9,32 @@ function generateHeader() {
 
   header.appendChild(name);
   console.log(header)
-  // header.appendChild(generateNav());
+  header.appendChild(generateNav());
 
   return header;
 };
 
 // Generate navbar
+function generateNav () {
+  const nav = document.createElement('nav');
+  const list = document.createElement('ul');
+
+  const menuItems = ['home', 'menu', 'contact'];
+
+  menuItems.forEach(item => {
+    const listItem = document.createElement('li');
+    const listLink = document.createElement('a');
+    listLink.textContent = item;
+    listLink.setAttribute('href', `#${item}`);
+    listItem.appendChild(listLink);
+    list.appendChild(listItem);
+  })
+  console.log(list)
+
+  nav.appendChild(list);
+
+  return nav;
+}
 
 export function initializeWebsite() {
   const content = document.querySelector('#content');
